9296d2f801e4ea92bfcc5c9e83cc78b1eb292444
feden2906 <feden2906@gmail.com>
2021-09-27 23:59:12 +0300

    qqq

---
+++ routes/customer.router.js
@@ -0,0 +1,29 @@
+onst router = require('express').Router();
+
+const { userController } = require('../controllers');
+
+
+
+router.get(
+    '/',
+    userController.getAllUsers
+);
+router.post(
+    '/',
+    userController.createUser
+);
+
+router.get(
+    '/:user_id',
+    userController.getSingleUser
+);
+router.delete(
+    '/:user_id',
+    userController.deleteUser
+);
+router.put(
+    '/:user_id',
+    userController.updateUser
+);
+
+module.exports = router;
