ae9c0fba9da2ba18e530e505f2bd8d90f022604a
feden2906 <feden2906@gmail.com>
2021-09-28 00:50:45 +0300

    next step create migration

--- routes/customer.router.js
+++ routes/customer.router.js
@@ -1,29 +1,38 @@
-onst router = require('express').Router();
+const { Customer } = require("../models");
+const { Order } = require("../models");
 
-const { userController } = require('../controllers');
+const router = require('express').Router();
 
-
-
-router.get(
-    '/',
-    userController.getAllUsers
-);
-router.post(
-    '/',
-    userController.createUser
+router.get('/', (req, res, next) => {
+  console.log(1111)
+    }
 );
 
-router.get(
-    '/:user_id',
-    userController.getSingleUser
-);
-router.delete(
-    '/:user_id',
-    userController.deleteUser
-);
-router.put(
-    '/:user_id',
-    userController.updateUser
+router.post('/', (req, res, next) => {
+  let customerId = null;
+  Customer.forge({ name: "Joey Tribianni", email: "jt@gmail.com" })
+      .save()
+      .then((customer) => {
+        customerId = customer.id;
+        return Order.forge({ total: 45, customer_id: customerId }).save();
+      })
+      .then((order) => {
+        return Order.forge({ total: 55, customer_id: customerId }).save();
+      })
+      .then((order) => {
+        return Customer.where({ id: customerId }).fetch({
+          withRelated: ['orders'], require: true
+        });
+      })
+      .then(result => {
+        console.clear();
+        console.log("Result is : ", result.toJSON());
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  res.json('GJ');
+    }
 );
 
 module.exports = router;
--- utils/database.js
+++ utils/database.js
@@ -2,10 +2,10 @@ const knex = require('knex')({
   client: 'mysql',
   connection: {
     host     : '127.0.0.1',
+    port     : 3306,
     user     : 'root',
     password : 'root',
-    database : 'ks-store',
-    charset  : 'utf8'
+    database : 'ks-store'
   }
 });
 
